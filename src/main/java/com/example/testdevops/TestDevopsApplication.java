6f10306581a1c736a08cbc25bb2123a2a24f7db5
Lior Lavon <>
2020-07-18 15:38:54 +0200

    add test devops

---
+++ src/main/java/com/example/testdevops/TestDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.example.testdevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDevopsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestDevopsApplication.class, args);
+    }
+
+}
